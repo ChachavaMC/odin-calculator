4e72a0a8a5568db53dd9d94760645cdd04425f1d
ChachavaMC <43357764+ChachavaMC@users.noreply.github.com>
2024-12-13 16:43:00 +0000

    Working calculator I may have skipped a few commits, but I got frustrated and just wanted to get it working.

--- script.js
+++ script.js
@@ -1,59 +1,69 @@
 let inputNum1 = "";
 let inputNum2 = "";
 let inputOperator = "";
-const mainDisplay = document.querySelector("div#calcScreenMain");
-const historyDisplay = document.querySelector("div#calcScreenHistory");
+let currentInput = "";
 
-//  Event Listeners
+//  EVENT LISTENERS
 //  Digits (including decimal point)
 document.querySelectorAll("div.calcDigit").forEach((elem) => {
     elem.addEventListener("click", (e) => {
-        console.log(e.target.textContent);
-        if(mainDisplay.textContent.length <= 10){
-            let newDisplay = mainDisplay.textContent + e.target.textContent;
-            updateMainDisplay(newDisplay);
-        }
+        currentInput += e.target.textContent;
+        updateMainDisplay(currentInput);
     })
 })
 //  Operators
 document.querySelectorAll("div.calcOperator").forEach((elem) => {
     elem.addEventListener("click", (e) => {
         const operator = e.target.textContent;
-        if (!inputNum1){
-            inputOperator = operator;
-            inputNum1 = mainDisplay.textContent;
-            updateMainDisplay("");
-            updateHistoryDisplay();
-        } else {
-            //  There is something in inputNum1
-            if(mainDisplay.textContent){
-                // If inputNum1 is there and a new number has been typed in
-                // assign the new number to inputNum2 and run the operate function
-                inputNum2 = mainDisplay.textContent;
-                updateHistoryDisplay();
-                result = operate(inputNum1, inputNum2, inputOperator);
-                updateMainDisplay(result);
-            } else {
+        if(currentInput){
+            if(!inputNum1){
+                inputNum1 = parseFloat(currentInput);
+                currentInput = "";
                 inputOperator = operator;
+                updateMainDisplay("");
+                updateHistoryDisplay()
+            } else {
+                inputNum2 = parseFloat(currentInput);
+                currentInput = "";
+                const result = operate(inputNum1, inputNum2, inputOperator)
+                updateMainDisplay(result);
                 updateHistoryDisplay();
+                inputNum1 = result;
+                inputOperator = operator;
+            }
+        } else {
+            if(inputNum1){
+                inputOperator = operator;
             }
         }
     })
 })
 // Equals
 document.querySelector("div.calcEquals").addEventListener("click", (e) => {
-    console.log(e.target.textContent);
+    if(inputNum1 && inputOperator && currentInput){
+        inputNum2 = parseFloat(currentInput);
+        const result = operate(inputNum1, inputNum2, inputOperator);
+        updateMainDisplay(result);
+        updateHistoryDisplay();
+        inputNum1 = result;
+        inputNum2 = "";
+        inputOperator = "";
+        currentInput = "";
+    }
 })
 //  All Clear
 document.querySelector("div.calcAllClear").addEventListener("click", (e) => {
-    console.log(e.target.textContent);
     initCalc();
 })
 //  Clear digit (basically acts as a delete key)
-document.querySelector("div#calcClear", (e) => {
-    console.log(e.target.textContent);
-})
+document.querySelector("div.calcClear").addEventListener("click", (e) => {
+    if(currentInput){
+        currentInput = currentInput.slice(0, -1);
+        updateMainDisplay(currentInput);
+    }
+});
 
+//  FUNCTIONS
 function calcAdd(a, b){
     return a + b;
 }
@@ -61,41 +71,55 @@ function calcSubtract(a, b){
     return a - b;
 }
 function calcMultiply(a, b){
-    return a * b;
+    return Math.round((a * b) * 100 ) / 100;
 }
 function calcDivide(a, b){
-    return a / b;
+    if(a === 0 || b === 0){
+        alert("Naughty naughty, no dividing by 0")
+        initCalc();
+        return;
+    }
+    return Math.round((a / b) * 100 ) / 100;
+}
+function calcPercent(a, b){
+    return Math.round(((a/100) * b) * 100) / 100
 }
 function operate(num1, num2, operator){
     let result = 0;
     switch(operator){
         case "+" :
-            return calcAdd(num1, num2);
+            result = calcAdd(num1, num2);
             break;
         case "-" :
-            return calcSubtract(num1, num2);
+            result = calcSubtract(num1, num2);
             break;
         case "*" :
-            return calcMultiply(num1, num2);
+            result = calcMultiply(num1, num2);
             break;
         case "/" :
-            return calcDivide(num1, num2)
+            result = calcDivide(num1, num2)
+            break;
+        case "%" :
+            result = calcPercent(num1, num2);
             break;
         default :
-            return "80085";
+            result = "80085";
     }
+    return result;
 }
-
 function initCalc(){
     inputNum1 = "";
     inputNum2 = "";
     inputOperator = "";
+    currentInput = "";
     updateMainDisplay("");
     updateHistoryDisplay();
 }
 function updateMainDisplay(str){
+    const mainDisplay = document.querySelector("div#calcScreenMain");
     mainDisplay.textContent = str;
 }
-function updateHistoryDisplay(){
-    historyDisplay.textContent = `${inputNum1} ${inputOperator} ${inputNum2}`
+function updateHistoryDisplay(){    
+    const historyDisplay = document.querySelector("div#calcScreenHistory");
+    historyDisplay.textContent = `${inputNum1} ${inputOperator} ${inputNum2}`;
 }
